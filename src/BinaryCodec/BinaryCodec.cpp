ea498e1cce7c714badb8d5c7dd6a70df1fad2fee
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-10-12 13:26:46 +0200

    [?+] [Serialization.Binary] Untested binary codec implementation.

---
+++ src/BinaryCodec/BinaryCodec.cpp
@@ -0,0 +1,102 @@
+// Binary coder/decoder for DSR's data using binary format
+
+/*
+  Author: Martin Eden
+  Last mod.: 2025-10-12
+*/
+
+/*
+  Used to store captured signal in EEPROM
+*/
+
+/*
+  Data structure
+
+  We're using binary representations.
+
+    <NumEvents: TUint_2>
+    (<TDuration>) * <NumEvents>
+*/
+
+#include <me_DigitalSignalRecorder.h>
+
+#include <me_BaseTypes.h>
+#include <me_StreamTools.h>
+#include <me_StreamsCollection.h>
+
+using namespace me_DigitalSignalRecorder::BinaryCodec;
+
+// Save signals to stream in binary format
+TBool me_DigitalSignalRecorder::BinaryCodec::Save(
+  TDigitalSignalRecorder * Dsr,
+  IOutputStream * OutputStream
+)
+{
+  TUint_2 NumEvents;
+  TAddressSegment DataAddrseg;
+  me_StreamsCollection::TWorkmemInputStream SourceDataStream;
+  TUint_2 EventIndex;
+  TSignalEvent Event;
+
+  if (!Dsr->GetNumEvents(&NumEvents))
+    return false;
+
+  DataAddrseg = { (TAddress) &NumEvents, sizeof(NumEvents) };
+  SourceDataStream.Init(DataAddrseg);
+
+  if (!me_StreamTools::CopyStreamTo(&SourceDataStream, OutputStream))
+    return false;
+
+  for (EventIndex = 1; EventIndex <= NumEvents; ++EventIndex)
+  {
+    if (!Dsr->GetEvent(&Event, EventIndex))
+      return false;
+
+    DataAddrseg = { (TAddress) &Event, sizeof(Event) };
+    SourceDataStream.Init(DataAddrseg);
+
+    if (!me_StreamTools::CopyStreamTo(&SourceDataStream, OutputStream))
+      return false;
+
+    EventIndex = EventIndex + 1;
+  }
+
+  return true;
+}
+
+// Load signals in binary format from stream
+TBool me_DigitalSignalRecorder::BinaryCodec::Load(
+  TDigitalSignalRecorder * Dsr,
+  IInputStream * InputStream
+)
+{
+  TUint_2 NumEvents;
+  TAddressSegment DataAddrseg;
+  me_StreamsCollection::TWorkmemOutputStream DestDataStream;
+  TUint_2 EventIndex;
+  TSignalEvent Event;
+
+  DataAddrseg = { (TAddress) &NumEvents, sizeof(NumEvents) };
+  DestDataStream.Init(DataAddrseg);
+
+  if (!me_StreamTools::CopyStreamTo(InputStream, &DestDataStream))
+    return false;
+
+  for (EventIndex = 1; EventIndex <= NumEvents; ++EventIndex)
+  {
+    DataAddrseg = { (TAddress) &Event, sizeof(Event) };
+    DestDataStream.Init(DataAddrseg);
+
+    if (!me_StreamTools::CopyStreamTo(InputStream, &DestDataStream))
+      return false;
+
+    if (!Dsr->Add(Event))
+      return false;
+  }
+
+  return true;
+}
+
+/*
+  2025-10-12
+*/
