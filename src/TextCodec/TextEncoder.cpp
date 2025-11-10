8f219196c40e4423c4d62008fa44d260afe2ede7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-11-10 11:04:12 +0200

    [!/] [Text encoder] Changed serialization format to length-prefixed..
    Those parentheses looks nice but when parsing them, their grammar
    requires "choice" construction. And to implement that we need
    re-readable input stream. Which is not the case with our UART stream.

--- src/TextCodec/TextEncoder.cpp
+++ src/TextCodec/TextEncoder.cpp
@@ -2,17 +2,76 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-11-09
+  Last mod.: 2025-11-10
 */
 
 #include <me_DigitalSignalRecorder.h>
 
 #include <me_BaseTypes.h>
 #include <me_BaseInterfaces.h>
-#include <me_Console.h>
-#include <me_DebugPrints.h>
+#include <me_BooleansCodec.h>
+#include <me_WriteInteger.h>
 
-using namespace me_DigitalSignalRecorder::TextCodec::Freetown;
+using namespace me_DigitalSignalRecorder;
+
+/*
+  Data format
+
+  We would prefer parenthesis format like
+
+    ( ( YES ( 1 320 ) ) ( NO ( 340 ) ) )
+
+  But it requires restorable input stream to parse.
+
+  So we're using length-prefixed format:
+
+    2
+    YES 0 0 1 320
+    NO 0 0 0 340
+*/
+
+// Emit newline
+static void EndLine(
+  IOutputStream * OutputStream
+)
+{
+  OutputStream->Write('\n');
+}
+
+// Emit space
+static void Space(
+  IOutputStream * OutputStream
+)
+{
+  OutputStream->Write(' ');
+}
+
+// Write duration to output stream
+static void SerializeDuration(
+  me_Duration::TDuration Duration,
+  IOutputStream * OutputStream
+)
+{
+  me_WriteInteger::Write_U2(Duration.KiloS, OutputStream);
+  Space(OutputStream);
+  me_WriteInteger::Write_U2(Duration.S, OutputStream);
+  Space(OutputStream);
+  me_WriteInteger::Write_U2(Duration.MilliS, OutputStream);
+  Space(OutputStream);
+  me_WriteInteger::Write_U2(Duration.MicroS, OutputStream);
+}
+
+// Write signal to output stream
+static void SerializeSignal(
+  TSignal Signal,
+  IOutputStream * OutputStream
+)
+{
+  me_BooleansCodec::Write(Signal.IsOn, OutputStream);
+  Space(OutputStream);
+  SerializeDuration(Signal.Duration, OutputStream);
+  EndLine(OutputStream);
+}
 
 /*
   Save signals to some loadable format
@@ -22,59 +81,25 @@ void me_DigitalSignalRecorder::TextCodec::Save(
   IOutputStream * OutputStream
 )
 {
-  IOutputStream * OrigOutputStream;
+  TUint_2 NumSignals;
   TUint_2 Index;
   TSignal Signal;
 
-  OrigOutputStream = Console.GetOutputStream();
+  NumSignals = Dsr->GetNumSignals();
 
-  Console.SetOutputStream(OutputStream);
+  me_WriteInteger::Write_U2(NumSignals, OutputStream);
+  EndLine(OutputStream);
 
-  Console.Print("(");
-  Console.Indent();
-
-  Index = 1;
-  while (Dsr->GetSignal(&Signal, Index))
+  for (Index = 1; Index <= NumSignals; ++Index)
   {
+    Dsr->GetSignal(&Signal, Index);
+
     SerializeSignal(Signal, OutputStream);
-    Index = Index + 1;
   }
-
-  Console.Unindent();
-  Console.Print(")");
-
-  Console.SetOutputStream(OrigOutputStream);
-}
-
-/*
-  Write signal to output stream in some convenient format
-*/
-void me_DigitalSignalRecorder::TextCodec::Freetown::SerializeSignal(
-  TSignal Signal,
-  IOutputStream * OutputStream
-)
-{
-  /*
-    We're lazy and smart here
-
-    We'll use Console to write to our stream.
-  */
-  IOutputStream * OrigOutputStream;
-
-  OrigOutputStream = Console.GetOutputStream();
-
-  Console.SetOutputStream(OutputStream);
-
-  Console.Write("(");
-  Console.Print(Signal.IsOn);
-  me_DebugPrints::PrintDuration(Signal.Duration);
-  Console.Write(")");
-  Console.EndLine();
-
-  Console.SetOutputStream(OrigOutputStream);
 }
 
 /*
   2025 # # #
   2025-10-12
+  2025-11-10
 */
